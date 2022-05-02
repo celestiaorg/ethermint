3b81989dd7cf06517024142c8fbfdc7223c1ad2a
Josh Bowen <josh@celestia.org>
2022-05-02 13:08:01 -0600

    modify init to handle existing genesis.json

--- docker/entrypoint-debug.sh
+++ docker/entrypoint-debug.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-set -e
+set -e 
 
-FILE=/ethermint/config/genesis.json
-
-if [ ! -f "$FILE" ] && [ "$1" = 'dlv' ]; then
+if [ "$1" = 'dlv' ]; then
     ./init.sh
 
     exec "$@" "--"
-elif [ "$1" = 'dlv' ]; then
-    exec "$@" "--"
 fi
-
-exec "$@"
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,14 +2,8 @@
 
 set -e 
 
-FILE=/ethermint/config/genesis.json
-
-if [ ! -f "$FILE" ] && [ "$1" = 'ethermintd' ]; then
+if [ "$1" = 'ethermintd' ]; then
     ./init.sh
 
     exec "$@" "--"
-elif [ "$1" = 'ethermintd' ]; then
-    exec "$@" "--"
 fi
-
-exec "$@"
